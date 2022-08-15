20b55d8a8895786f39a406238c631bef0448c026
nekix <44142501+nekix@users.noreply.github.com>
2022-08-15 19:33:43 +0300

    Task 5: Use the Visual Studio Debugger to step through the code

--- School/MainWindow.xaml.cs
+++ School/MainWindow.xaml.cs
@@ -78,6 +78,7 @@ namespace School
                         saveChanges.IsEnabled = true;
                     }
 
+                    //Task 5: Use the Visual Studio Debugger to step through the code
                     break;
 
                 default:
