d243e3db0a704e271c079e0028136306cd47d61a
nekix <44142501+nekix@users.noreply.github.com>
2022-08-16 18:26:44 +0300

    Task 2: Define the LogonSuccess event and add dummy code for the Logon_Click event

--- GradesPrototype/Views/LogonPage.xaml.cs
+++ GradesPrototype/Views/LogonPage.xaml.cs
@@ -28,11 +28,25 @@ namespace GradesPrototype.Views
         }
 
         #region Event Members
-        // TODO: Exercise 1: Task 2a: Define the LogonSuccess event handler
+        public event EventHandler LogonSuccess;
 
         #endregion
 
         #region Logon Validation
+
+        //Init logon process
+        public void Logon_Click(object sender, RoutedEventArgs e)
+        {
+            SessionContext.UserName = username.Text;
+
+            //Set dummy current student
+            if ((SessionContext.UserRole = (bool)userrole.IsChecked ? Role.Teacher : Role.Student) == Role.Student)
+            {
+                SessionContext.CurrentStudent = "Eric Gruber";
+            }
+
+            LogonSuccess?.Invoke(this, null);
+        }
 
         // TODO: Exercise 1: Task 2b: Implement the Logon_Click event handler for the Logon button
         // Simulate logging on (no validation or authentication performed yet)
