97e5e60e5d32f5a710d11ea15bac1a524942ac7a
nekix <44142501+nekix@users.noreply.github.com>
2022-08-16 11:55:55 +0300

    Task 5: Debug the application and step into the new method calls

--- School/MainWindow.xaml.cs
+++ School/MainWindow.xaml.cs
@@ -84,7 +84,7 @@ namespace School
 
                         saveChanges.IsEnabled = true;
                     }
-                 
+                    
                     break;
 
                     //Add new student 
