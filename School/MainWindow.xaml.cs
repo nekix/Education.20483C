e9780bb26fb6e3624393d4bc5f24ae69a58be737
nekix <44142501+nekix@users.noreply.github.com>
2022-08-16 11:57:51 +0300

    Task 1: Run the application and observe that student details that are not valid can be entered

--- School/MainWindow.xaml.cs
+++ School/MainWindow.xaml.cs
@@ -133,7 +133,7 @@ namespace School
         // Save changes back to the database and make them permanent
         private void saveChanges_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         #endregion
