5efa58ff03eed294ca7118eb917190374c4a03c0
nekix <44142501+nekix@users.noreply.github.com>
2022-08-17 13:48:58 +0300

    Task 3: Change the Students ArrayList collection into a List\<Student\> collection

--- GradesPrototype/Data/DataSource.cs
+++ GradesPrototype/Data/DataSource.cs
@@ -11,7 +11,7 @@ namespace GradesPrototype.Data
     {
         // Collections holding the data used by the prototype application
         public static ArrayList Teachers;
-        public static ArrayList Students;
+        public static List<Student> Students;
         public static ArrayList Grades;
         public static List<string> Subjects;
 
@@ -35,7 +35,7 @@ namespace GradesPrototype.Data
                 new Teacher() { TeacherID = 3, UserName = "newmanb", Password = "password", FirstName = "Belinda", LastName = "Newman", Class = "2A" }
             };
 
-            Students = new ArrayList()
+            Students = new List<Student>()
             {
                 new Student() { StudentID = 1, UserName = "liuk", Password = "password", TeacherID = 1, FirstName = "Kevin", LastName = "Liu" },
                 new Student() { StudentID = 2, UserName = "weberm", Password = "password", TeacherID = 1, FirstName = "Martin", LastName = "Weber" },
