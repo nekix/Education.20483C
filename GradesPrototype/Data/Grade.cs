60654c716b119bfff535f18017443a359215892f
nekix <44142501+nekix@users.noreply.github.com>
2022-08-17 10:43:28 +0300

    Task 1: Convert the Grades struct into a class

--- GradesPrototype/Data/Grade.cs
+++ GradesPrototype/Data/Grade.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Security.RightsManagement;
 using System.Text;
@@ -8,12 +9,30 @@ using System.Threading.Tasks;
 namespace GradesPrototype.Data
 {
 
-    public struct Grade
+    public class Grade
     {
         public int StudentID { get; set; }
         public string AssessmentDate { get; set; }
         public string SubjectName { get; set; }
         public string Assessment { get; set; }
         public string Comments { get; set; }
+
+        public Grade()
+        {
+            StudentID = 0;
+            AssessmentDate = DateTime.Now.ToString("d");
+            SubjectName = "Math";
+            Assessment = "A";
+            Comments = string.Empty;
+        }
+
+        public Grade(int studentID, string assessmentDate, string subjectName, string assessment, string comments)
+        {
+            StudentID = studentID;
+            AssessmentDate = assessmentDate;
+            SubjectName = subjectName;
+            Assessment = assessment;
+            Comments = comments;
+        }
     }
 }
