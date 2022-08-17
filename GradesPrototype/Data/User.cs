7c9f60654a49acecef401f51b20391f977108bfd
nekix <44142501+nekix@users.noreply.github.com>
2022-08-17 18:52:22 +0300

    Task 1: Create the User abstract base class

---
+++ GradesPrototype/Data/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradesPrototype.Data
+{
+    public abstract class User
+    {
+        public string UserName { get; set; }
+
+        private string _password = Guid.NewGuid().ToString();
+        public string Password
+        {
+            set { _password = value; }
+        }
+
+        public bool VerifyPassword(string Password)
+        {
+            return string.Compare(Password, _password, StringComparison.Ordinal) == 0;
+        }
+    }
+}
