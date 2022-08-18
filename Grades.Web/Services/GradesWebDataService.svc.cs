9ba8beb5b6ea0da75d26e20722987fbb74f6fbc0
nekix <44142501+nekix@users.noreply.github.com>
2022-08-18 19:25:53 +0300

    Task 3: Add an operation to retrieve all of the students in a specified class

--- Grades.Web/Services/GradesWebDataService.svc.cs
+++ Grades.Web/Services/GradesWebDataService.svc.cs
@@ -12,20 +12,28 @@ using Grades.DataModel;
 namespace Grades.Web.Services
 {
     //TODO: Excersice 1: Task 2a: Replace the object keyword with your data source class name.
-    public class GradesWebDataService : DataService<object>
+    public class GradesWebDataService : DataService<SchoolGradesDBEntities>
     {
         public static void InitializeService(DataServiceConfiguration config)
         {
+            config.SetServiceOperationAccessRule(nameof(StudentsInClass), ServiceOperationRights.AllRead)
+
             config.SetEntitySetAccessRule("Grades", EntitySetRights.All);
             config.SetEntitySetAccessRule("Teachers", EntitySetRights.All);
             config.SetEntitySetAccessRule("Students", EntitySetRights.All);
             config.SetEntitySetAccessRule("Subjects", EntitySetRights.All);
             config.SetEntitySetAccessRule("Users", EntitySetRights.All);
-
-            //TODO: Excersice 1: Task 2b: set rules to indicate which entity sets and service operations are visible, updatable, etc
-
         }
 
+        [WebGet]
+        public IEnumerable<Student> StudentsInClass(string className)
+        {
+            var result = from s in CurrentDataSource.Students
+                         where s.Teacher.Class == className
+                         select s;
+
+            return result;
+        }
     }
 
 
